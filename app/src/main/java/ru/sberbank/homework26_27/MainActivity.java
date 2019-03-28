c23f3680ec180b5832a67a9320698cb92a64da6d
Alexander <sapfirmoscow@yandex.ru>
2019-03-28 23:07:44 +0300

    migrate to androidx and Java 8 language features

--- app/src/main/java/ru/sberbank/homework26_27/MainActivity.java
+++ app/src/main/java/ru/sberbank/homework26_27/MainActivity.java
@@ -1,6 +1,6 @@
 package ru.sberbank.homework26_27;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
